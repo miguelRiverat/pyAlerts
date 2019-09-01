9164f8fc4a0dac822998364c7b144760894f6ff6
miguel <miguel@debian.baxten>
2019-09-01 15:53:28 -0500

    a

--- clasification.py
+++ clasification.py
@@ -61,99 +61,98 @@ def calcPres(listArr, listdates, present, clase_ter, corp, prod, fecha_lan, mole
     file.close()
 
 
-def init():
-    client = storage.Client()
-    blob = client.get_bucket('sanfer_alerts')
 
+client = storage.Client()
+blob = client.get_bucket('sanfer_alerts')
+
+now = datetime.now()
+print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"))
+lineArray = []
+
+client = bigquery.Client()
+sql_clases = """
+SELECT DISTINCT claseterapeuticanivel3, SPLIT(claseterapeuticanivel3, ' ')[OFFSET(0)] clase FROM sanfer.tb_details_sales  where Corporacion = 'SANFER CORP.'
+"""
+df_clases = client.query(sql_clases).to_dataframe()
+df_clases['clase'].values.tolist()     
+
+for index, row in df_clases.iterrows():
+    clase = row['clase']
+    
     now = datetime.now()
-    print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"))
-    lineArray = []
+    print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"),  '==========================> ', index, clase)
+    
+    sql_presentation = """
+    SELECT DISTINCT presentacion FROM sanfer.tb_details_sales  where claseterapeuticanivel3 like '%{0}%'
+    """.format(clase)
 
-    client = bigquery.Client()
-    sql_clases = """
-    SELECT DISTINCT claseterapeuticanivel3, SPLIT(claseterapeuticanivel3, ' ')[OFFSET(0)] clase FROM sanfer.tb_details_sales  where Corporacion = 'SANFER CORP.'
-    """
-    df_clases = client.query(sql_clases).to_dataframe()
-    df_clases['clase'].values.tolist()     
+    presentations = client.query(sql_presentation).to_dataframe()
+    procs = []
+    claseTer = []
 
-    for index, row in df_clases.iterrows():
-        clase = row['clase']
+    sql_pres_months = """
+                    SELECT presentacion, mthunidades, fechaventa, claseterapeuticanivel3, corporacion, producto, fechalanzamientopresentacion, moleculan1
+                    FROM sanfer.tb_details_sales 
+                    where presentacion in ("{0}") 
+                    group by presentacion, mthunidades, fechaventa, claseterapeuticanivel3, corporacion, producto, fechalanzamientopresentacion, moleculan1
+                    order by presentacion, fechaventa asc""".format('","'.join(presentations['presentacion'].tolist()))
+    months = client.query(sql_pres_months).to_dataframe()
+
+    for index, pres in presentations.iterrows():
+        present = pres['presentacion']
+        months_pres = months.loc[months['presentacion'] == present]
+        firsRow = months_pres.iloc[0]
+        clase_ter = firsRow['claseterapeuticanivel3']
+        corp = firsRow['corporacion']
+        prod = firsRow['producto']
+        fecha_lan = firsRow['fechalanzamientopresentacion']
+        molecula = firsRow['moleculan1']
+
+        listArr = months_pres['mthunidades'].values.reshape(len(months_pres), -1).tolist()
+        listdates = months_pres['fechaventa'].values.reshape(len(months_pres), -1).tolist()
         
-        now = datetime.now()
-        print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"),  '==========================> ', index, clase)
-        
-        sql_presentation = """
-        SELECT DISTINCT presentacion FROM sanfer.tb_details_sales  where claseterapeuticanivel3 like '%{0}%'
-        """.format(clase)
+        proc = Process(target=calcPres, args=(listArr, listdates, present, clase_ter, corp, prod, fecha_lan, molecula))
+        procs.append(proc)
+        proc.start()
 
-        presentations = client.query(sql_presentation).to_dataframe()
-        procs = []
-        claseTer = []
-
-        sql_pres_months = """
-                        SELECT presentacion, mthunidades, fechaventa, claseterapeuticanivel3, corporacion, producto, fechalanzamientopresentacion, moleculan1
-                        FROM sanfer.tb_details_sales 
-                        where presentacion in ("{0}") 
-                        group by presentacion, mthunidades, fechaventa, claseterapeuticanivel3, corporacion, producto, fechalanzamientopresentacion, moleculan1
-                        order by presentacion, fechaventa asc""".format('","'.join(presentations['presentacion'].tolist()))
-        months = client.query(sql_pres_months).to_dataframe()
-
-        for index, pres in presentations.iterrows():
-            present = pres['presentacion']
-            months_pres = months.loc[months['presentacion'] == present]
-            firsRow = months_pres.iloc[0]
-            clase_ter = firsRow['claseterapeuticanivel3']
-            corp = firsRow['corporacion']
-            prod = firsRow['producto']
-            fecha_lan = firsRow['fechalanzamientopresentacion']
-            molecula = firsRow['moleculan1']
-
-            listArr = months_pres['mthunidades'].values.reshape(len(months_pres), -1).tolist()
-            listdates = months_pres['fechaventa'].values.reshape(len(months_pres), -1).tolist()
-            
-            proc = Process(target=calcPres, args=(listArr, listdates, present, clase_ter, corp, prod, fecha_lan, molecula))
-            procs.append(proc)
-            proc.start()
-
-        for proc in procs:
-            proc.join()
+    for proc in procs:
+        proc.join()
 
 
-        nameFile = './clases/{}.csv'.format(clase)
+    nameFile = './clases/{}.csv'.format(clase)
 
-        with open(nameFile, 'wb') as outfile:
-            for filename in glob.glob('./csv/*.csv'):
-                with open(filename, 'rb') as readfile:
-                    shutil.copyfileobj(readfile, outfile)
-                os.remove(filename)
-        line_prepender(nameFile, 'tendencia;claseterapeutica;corporacion;producto;fechalanzamiento;presentacion;moleculan1;fecini;fecend;tipo')
-        
-        data = pd.read_csv(nameFile, delimiter=";")
-        for index, row in data.iterrows():
-            fecin = row['fecini']
-            fecen = row['fecend']
-            dates = data.loc[(data['fechalanzamiento'] >= fecin) & (data['fechalanzamiento'] <= fecen)]
-
-            if (row['tendencia'] == 'BAJA' and len(dates) > 1):
-                data.loc[index, "tipo"]  = 'lanzamiento'
-            else:
-                combin = data.loc[(data['fecini'] >= fecin) & (data['fecend'] <= fecen)]
-                maxi = combin['tendencia'].value_counts().index[0]
-                data.loc[index, "tipo"] = '{}, por demanda'.format(maxi)
-
-        data.to_csv('./process/{}.csv'.format(clase), sep=';', index=False, header=False)
-        os.remove(nameFile)
-        
-    with open(finalname, 'wb') as outfile:
-        for filename in glob.glob('./process/*.csv'):
+    with open(nameFile, 'wb') as outfile:
+        for filename in glob.glob('./csv/*.csv'):
             with open(filename, 'rb') as readfile:
                 shutil.copyfileobj(readfile, outfile)
             os.remove(filename)
+    line_prepender(nameFile, 'tendencia;claseterapeutica;corporacion;producto;fechalanzamiento;presentacion;moleculan1;fecini;fecend;tipo')
     
-    finalname = './final/mth.csv'
-    line_prepender(finalname, 'tendencia;claseterapeutica;corporacion;producto;fechalanzamiento;presentacion;moleculan1;fecini;fecend;tipo')
-    with open(finalname, 'rb') as uploadFile:
-        blob.upload_from_file(uploadFile)
+    data = pd.read_csv(nameFile, delimiter=";")
+    for index, row in data.iterrows():
+        fecin = row['fecini']
+        fecen = row['fecend']
+        dates = data.loc[(data['fechalanzamiento'] >= fecin) & (data['fechalanzamiento'] <= fecen)]
 
-    now = datetime.now()
-    print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"))
+        if (row['tendencia'] == 'BAJA' and len(dates) > 1):
+            data.loc[index, "tipo"]  = 'lanzamiento'
+        else:
+            combin = data.loc[(data['fecini'] >= fecin) & (data['fecend'] <= fecen)]
+            maxi = combin['tendencia'].value_counts().index[0]
+            data.loc[index, "tipo"] = '{}, por demanda'.format(maxi)
+
+    data.to_csv('./process/{}.csv'.format(clase), sep=';', index=False, header=False)
+    os.remove(nameFile)
+finalname = './final/mth.csv'
+with open(finalname, 'wb') as outfile:
+    for filename in glob.glob('./process/*.csv'):
+        with open(filename, 'rb') as readfile:
+            shutil.copyfileobj(readfile, outfile)
+        os.remove(filename)
+
+line_prepender(finalname, 'tendencia;claseterapeutica;corporacion;producto;fechalanzamiento;presentacion;moleculan1;fecini;fecend;tipo')
+with open(finalname, 'rb') as uploadFile:
+    blob.upload_from_file(uploadFile)
+
+now = datetime.now()
+print("date and time =", now.strftime("%d/%m/%Y %H:%M:%S"))
